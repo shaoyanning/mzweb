b8c13a40987366a83d9473f7889c1bd38b8b4e9a
Administrator <Administrator@DESKTOP-D7Q7Q9F.mshome.net>
2018-07-29 16:55:27 +0800

    实现getById查询操作

--- src/cn/dw/dao/ProductDao.java
+++ src/cn/dw/dao/ProductDao.java
@@ -2,6 +2,7 @@ package cn.dw.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.mysql.jdbc.StreamingNotifiable;
@@ -14,16 +15,61 @@ public class ProductDao extends BaseDao {
 
 	public static void main(String[] args) {
 		ProductDao dao = new ProductDao();
-//		Product product = new Product();
-//		product.setId(4);
-//		product.setName("小米手机4");
-//		product.setPrice(2400.00);
-//		product.setRemark("商品备注4！");
-//		 System.out.println(dao.update(product));
-//		 int result = dao.save(product);
+		// Product product = new Product();
+		// product.setId(4);
+		// product.setName("小米手机4");
+		// product.setPrice(2400.00);
+		// product.setRemark("商品备注4！");
+		// System.out.println(dao.update(product));
+		// int result = dao.save(product);
 		// System.out.println("result:" + result);
-		int result = dao.delete(4);
-		System.out.println(result);
+//		int result = dao.delete(4);
+//		System.out.println(result);
+		Product product = dao.getById(100);
+		// 默认调用toString方法
+		System.out.println(product);
+	}
+
+	public Product getById(int id) {
+		Product product = null;
+		String sql = "select * from product where id = ?";
+		// 1: 获取connection对象
+		JdbcUtils utils = new JdbcUtils();
+		Connection conn = null;
+		PreparedStatement pre = null;
+		// 查询的结果集(存储了的就是返回table结构)
+		ResultSet rs = null;
+		try {
+			conn = utils.getConnection();
+			pre = conn.prepareStatement(sql);
+			pre.setInt(1, id);
+			// rs存储了结果集,即使没有数据,也存储了空结果集
+			rs = pre.executeQuery();
+			// rs光标(数据库的游标),默认在第一行之前,因为根据id主键查询,最多查询出一条记录(if)
+			if (rs.next()) { // next() 把当前光标向下移动一行,如果当前行有记录则返回的true
+				// 数据库的每条记录,对应Java的对象
+				product = new Product();
+				// getInt("id") 获取id列的值并且以int类型
+				product.setId(rs.getInt("id"));
+				product.setPrice(rs.getDouble("price"));
+				product.setDate(rs.getDate("date"));
+				product.setRemark(rs.getString("remark"));
+				product.setName(rs.getString("name"));
+			}
+			return product;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}finally {
+			try {
+				rs.close();
+				pre.close();
+				conn.close();
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		
 	}
 
 	public int update(Product product) {
--- src/cn/dw/model/Product.java
+++ src/cn/dw/model/Product.java
@@ -3,7 +3,7 @@ package cn.dw.model;
 import java.util.Date;
 
 // 实体类：一般与数据的的表对应  类 --> 表   对象 --> 记录
-public class Product {
+public class Product extends Object{
 	
 	private Integer id;
 	private String name;
@@ -41,7 +41,9 @@ public class Product {
 	public void setDate(Date date) {
 		this.date = date;
 	}
-	
-	
-
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + ", remark=" + remark + ", date=" + date
+				+ "]";
+	}
 }
