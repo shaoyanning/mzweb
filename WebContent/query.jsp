67f2ef0460550bf7becfc9c6c9c87ac3fa2d955d
Administrator <574231288@qq.com>
2018-07-31 09:48:13 +0800

    1:讲解了转发与重定向的区别 2:lib中加载了两个jstl标签库

--- WebContent/query.jsp
+++ WebContent/query.jsp
@@ -5,11 +5,22 @@
 <title>查询商品页面</title>
 </head>
 <body>
-    
+    <!--  request,session,application都称为内置对象 -->
     <!-- web.jsp 应该吧数据传递给productService,但是service不能接受http请求,因此java出现了servlet -->
 	<form action="<%=request.getContextPath()%>/ProductServlet" method="get">
 		商品名称:<input type="text" name="keyword" /><br />
 		<button type="submit">提交商品</button>
 	</form>
+	<table width="600">
+		<tr>
+			<th>编码</th>
+			<th>名称</th>
+			<th>价格</th>
+			<th>备注</th>
+			<th>操作</th>
+		</tr>
+		<!-- 有多少结果集对象,则循环多少次,前端Java有JSTL标签可以获取request内置对象中的数据 -->
+		
+	</table>
 </body>
 </html>
--- src/cn/dw/servlet/ProductServlet.java
+++ src/cn/dw/servlet/ProductServlet.java
@@ -3,6 +3,7 @@ package cn.dw.servlet;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,18 +27,21 @@ public class ProductServlet extends HttpServlet {
 		super();
 	}
 
-	// 只能用来处理get请求
+	// 只能用来处理get请求   <a>也是get请求
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// 1: 获取查询的关键字 keyword
 		String keyword = request.getParameter("keyword");
 		// 2: 调用业务逻辑(此处不需要添加%%)
 		ArrayList<Product> proList = productService.queryByName(keyword);
-		for(Product temp:proList) {
-			System.out.println(temp);
-		}
-		// 3: 跳转到查询页面,然后显示数据
-		response.sendRedirect(request.getContextPath() + "/query.jsp");
+		request.setAttribute("proList", proList);
+		// 3: 重定向,让用户再去访问query.jsp (会生成一个新request对象)
+//		response.sendRedirect(request.getContextPath() + "/query.jsp");
+		// 转发请求,会把之前的request请求转发第二个页面,只能转发到系统内部资源(默认添加工程名)
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/query.jsp");
+		// 共享第一次请求的request对象
+		dispatcher.forward(request, response);
+		
 	}
 
 	// 处理 post请求
@@ -55,7 +59,6 @@ public class ProductServlet extends HttpServlet {
 		// 2: 调用业务逻辑
 		productService.save(product);
 		// 3: 跳转到查询页面
-		System.out.println();
 //		response.sendRedirect("/mzweb/query.jsp");
 		response.sendRedirect(request.getContextPath() + "/query.jsp");
 	}
